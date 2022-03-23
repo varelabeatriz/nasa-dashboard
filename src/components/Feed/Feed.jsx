1b9db303834dc617ecfafe15f96bb04ec064d140
Beatriz Varela <beatrizsv19@gmail.com>
2022-03-23 20:38:25 -0300

    first commit

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,38 @@
+import Axios from 'axios';
+import React, { useState } from 'react';
+
+export function Feed (){
+
+    const [image, setImage ] = useState([]);
+    const [imageUrl, setImageUrl ] = useState('');
+    const [date, setDate ] = useState('2001-04-19');
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=n8y6D1t17guEP26zaoJpQRuIbkihNKIgFf1S3baD&date=${date}`
+
+    const getData = async () => {
+        const result = await Axios.get(url);
+        setImage(result);
+        setImageUrl(image.data.hdurl);
+    }
+
+    return (
+        <div>
+            <p>Hello World</p>
+
+            <label htmlFor="date"></label>
+            <input type="text" name='date'/>
+
+            <button onClick={()=> {getData()}}>Get Data</button>
+
+            {image ? (
+                <div>
+                    <p>Got image</p>
+                    <img src={imageUrl} alt="" />
+                </div>
+            ) : (
+                <p>No image</p>
+            )}
+
+        </div>
+    )
+}
